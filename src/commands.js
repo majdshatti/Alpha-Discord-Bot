79db24cfd50eda92b5c6c3622483a329a396755c
majdshatti <majdshatti8@gmail.com>
2022-09-02 19:01:07 +0300

    Slash commands added

---
+++ src/commands.js
@@ -0,0 +1,30 @@
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+require("dotenv").config();
+
+const commands = [];
+
+commands.push(
+  new SlashCommandBuilder()
+    .setName("activate")
+    .setDescription("Ativates daily morning greetings")
+);
+
+commands.push(
+  new SlashCommandBuilder().setName("quote").setDescription("Get a quote")
+);
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: commands,
+    });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
